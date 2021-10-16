2377a7c90d18bd92a90852ee7fd7668fc7d48447
LeoFC97 <leonardo.fc2@outlook.com>
2021-10-16 01:16:37 -0300

    feat: add new endpoint and test

--- recipe-apito/src/app/api/http/routes.ts
+++ recipe-apito/src/app/api/http/routes.ts
@@ -3,10 +3,11 @@ import container from '../../container';
 import { adapt } from '../../drivers/http/express-router-adapter';
 
 import HealthController from './controllers/health';
-
+import FoodJokeController from './controllers/getRandomFoodJoke';
 
 const router = express.Router();
 
 router.get('/health', adapt(container.resolve(HealthController)));
+router.get('/randomJoke', adapt(container.resolve(FoodJokeController)));
 
 export default router;
