53f3275230305bcf5c5e0f5f3f85145b4532408a
LeoFC97 <leonardo.fc2@outlook.com>
2021-10-16 02:43:29 -0300

    feat: add get by id repository

--- recipe-apito/src/app/interfaces/entities/foodJoke/foodJoke-repository.ts
+++ recipe-apito/src/app/interfaces/entities/foodJoke/foodJoke-repository.ts
@@ -3,4 +3,5 @@ import FoodJoke from './foodJoke';
 export default interface FoodJokeRepository {
   getRandom(): Promise<FoodJoke>;
   create(textJoke: string): Promise<FoodJoke>;
+  getById(jokeId: string): Promise<FoodJoke>;
 }
