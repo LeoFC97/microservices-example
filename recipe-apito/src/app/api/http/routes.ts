154e83b24687842fe0e7afb8ca0274c8755e1181
LeoFC97 <leonardo.fc2@outlook.com>
2021-10-16 02:01:51 -0300

    feat: add CreateFoodJokeController to routes

--- recipe-apito/src/app/api/http/routes.ts
+++ recipe-apito/src/app/api/http/routes.ts
@@ -3,11 +3,13 @@ import container from '../../container';
 import { adapt } from '../../drivers/http/express-router-adapter';
 
 import HealthController from './controllers/health';
-import FoodJokeController from './controllers/getRandomFoodJoke';
+import GetRandomFoodJokeController from './controllers/getRandomFoodJoke';
+import CreateFoodJokeController from './controllers/createFoodJoke';
 
 const router = express.Router();
 
 router.get('/health', adapt(container.resolve(HealthController)));
-router.get('/randomJoke', adapt(container.resolve(FoodJokeController)));
+router.get('/randomJoke', adapt(container.resolve(GetRandomFoodJokeController)));
+router.post('/joke', adapt(container.resolve(CreateFoodJokeController)));
 
 export default router;
